8073e05f8fad0abec5c35ebf664a472b085e9a49
Raphael Torres <rtcarva@gmail.com>
2023-06-19 17:10:28 -0300

    Create Desafio014.py
    Conversor de Celcius para Fahrenheit.

---
+++ Desafio014.py
@@ -0,0 +1,6 @@
+print('Desafio014: Crie um converso de temperatura')
+
+c = float(input('Digite o valor da temperatura em ºC: '))
+f = ((9 * c) /5) + 32
+
+print('A temperatura de {:.1f}ºC em Fahrenheit é {:.1f}ºF'.format(c,f))
